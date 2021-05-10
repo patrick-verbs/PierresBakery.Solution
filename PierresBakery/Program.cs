0eb859b00be80953170e698ac0803999d4dff24a
Patrick Lee <patricklee1138@live.com>
2021-05-10 00:05:07 -0700

    :lipstick: add UI/UX menu

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -8,12 +8,68 @@ namespace PierresBakeryNamespace
   {
     public static void Main()
     {
-      // UI Stuff
-      Console.WriteLine("hi I'm a console log" + "\n");
+      Console.WriteLine("Hi! Welcome to Pierre's Bakery!" + "\n");
 
-      Bread.SetBreadOrder("bread", 25);
-      Pastry.SetPastryOrder("pastry", 10);
+      Shop:
+      Console.WriteLine("Would you like to see our selection of bread, or pastries? " + "(Bread/Pastries/Checkout)" + "\n");
+      string userChoice = (Console.ReadLine()).ToLower();
+      if (userChoice[0] == 'b')
+      {
+        goto Bread;
+      }
+      else if (userChoice[0] == 'p')
+      {
+        goto Pastry;
+      }
+      else if (userChoice[0] == 'c')
+      {
+        goto Checkout;
+      }
 
+      Bread:
+      Console.WriteLine("How many loaves of bread would you like?");
+      int breadCount = Int32.Parse(Console.ReadLine());
+      Bread.SetBreadOrder("bread", breadCount);
+      if (breadCount > 1)
+      {
+        Console.WriteLine(breadCount + "loaves of bread! That's a lot of bread!");
+      }
+      else if (breadCount == 1)
+      {
+        Console.WriteLine("One loaf of bread for ya? Coming right up!");
+      }
+      goto Shop;
+
+      Pastry:
+      Console.WriteLine("How many pastries would you like?");
+      int pastryCount = Int32.Parse(Console.ReadLine());
+      Pastry.SetPastryOrder("pastry", pastryCount);
+      if (pastryCount > 1)
+      {
+        Console.WriteLine(pastryCount + "pastries! That's a lot of sugar!");
+      }
+      else if (pastryCount == 1)
+      {
+        Console.WriteLine("One tasty pastry for ya? Coming right up!");
+      }
+      goto Shop;
+
+      // welcome message
+      // view bread or pastry selections
+      // add bread
+      // - choose bread
+      // - exit
+      // add pastry
+      // - choose pastry
+      // - exit
+      // print order
+
+      Checkout:
+      PrintOrder();
+    }
+
+    public static void PrintOrder()
+    {
       Dictionary<string, Dictionary<string, int>> fullOrder = Order.GetFullOrder();
       int runningTotal = 0;
 
@@ -50,7 +106,7 @@ namespace PierresBakeryNamespace
         }
       }
 
-      Console.WriteLine("TOTAL: " + runningTotal);
+      Console.WriteLine("TOTAL: $" + runningTotal);
     }
   }
 }
