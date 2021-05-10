863928362f1023f9736fdb0930030ca926de884b
Patrick Lee <patricklee1138@live.com>
2021-05-09 19:05:49 -0700

    :lipstick: UI prints full bread & pastry order with discounted totals

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using PierresBakeryNamespace.Models;
 
 namespace PierresBakeryNamespace 
@@ -8,6 +9,48 @@ namespace PierresBakeryNamespace
     public static void Main()
     {
       // UI Stuff
+      Console.WriteLine("hi I'm a console log" + "\n");
+
+      Bread.SetBreadOrder("bread", 25);
+      Pastry.SetPastryOrder("pastry", 10);
+
+      Dictionary<string, Dictionary<string, int>> fullOrder = Order.GetFullOrder();
+      int runningTotal = 0;
+
+      foreach(var kvp in fullOrder)
+      {
+        runningTotal += fullOrder[kvp.Key]["subtotal"];
+
+        Console.WriteLine(kvp.Key.ToUpper());
+        Console.WriteLine("-----");
+        foreach (var nestedKvp in fullOrder[kvp.Key])
+        {
+          string padding = "";
+          foreach (char letter in nestedKvp.Key)
+          {
+            padding = "               ";
+            int difference = padding.Length - nestedKvp.Key.Length;
+            padding = padding.Substring(0, difference);
+          }
+          Console.WriteLine("   " + nestedKvp.Key + ":" + padding + nestedKvp.Value);
+        }
+        if (kvp.Key == "bread")
+        {
+          int discounts = Bread.breadDiscounts();
+          Console.WriteLine("   discounts:     -" + discounts);
+          Console.WriteLine("");
+          runningTotal -= discounts;
+        }
+        else if (kvp.Key == "pastry")
+        {
+          int discounts = Pastry.pastryDiscounts();
+          Console.WriteLine("   discounts:     -" + discounts);
+          Console.WriteLine("");
+          runningTotal -= discounts;
+        }
+      }
+
+      Console.WriteLine("TOTAL: " + runningTotal);
     }
   }
 }
